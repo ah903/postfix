2caeb783146f339d0d35953c942543ce66690521
ah903 <andrew.hall903@gmail.com>
2017-10-22 14:41:04 +0100

    Refactored to modules

--- js/lexar.js
+++ js/lexar.js
@@ -1,12 +1,81 @@
+/////////////////////////////////////////////////////////////////////////////////
+// Module 		: 		lexar.js
+// Description	: 		Simple postfix expression parser and evaluator
+//						Requires stack datatype as a dependnency which is
+//						injected in as the stack variable thourgh Require
+// Date			: 		Oct 2017
+// Author		:       Andrew Hall  
+/////////////////////////////////////////////////////////////////////////////////
+define(["stack"], function(stack) {
+	
+	var lexar = function(){
 
-function lexar(){
+		////////////////////////////////////////////////////////////////////////
+		// Private Member variables
+		////////////////////////////////////////////////////////////////////////
+		 var explainer = new Array();
 
 
-	this.isOperator = function(value){
-		return ("+-*/^".indexOf(value) >= 0);
-	}
+		////////////////////////////////////////////////////////////////////////
+		// Public Interface Evaluate
+		// Tokenises a postfix string and evaluates 
+		////////////////////////////////////////////////////////////////////////
+		this.evaluate = function(value){
+			var expressionStack = new stack();
+			var tokens = value.split(" "); 
+			explainer = [];
 
-	this.isOperand = function(value){
-		return !(isNaN(value))	
-	}
-}
+			for(i=0; i < tokens.length; i++){
+				if(this.isOperand(tokens[i])) {
+					expressionStack.push(tokens[i]);
+				}
+				else if(this.isOperator(tokens[i])){
+					if(expressionStack.length() >= 2){
+						var v2 = expressionStack.pop();
+						var v1 = expressionStack.pop();
+						var ev = v1 + tokens[i] + v2;
+						var result = eval(ev);
+						logEval(ev,result);
+						expressionStack.push(result);
+					}
+				}
+			}
+			return expressionStack.pop();
+		}
+
+
+		////////////////////////////////////////////////////////////////////////
+		// Public Interface IsOperator
+		// Tests whether an expression is a valid operator
+		////////////////////////////////////////////////////////////////////////
+		this.isOperator = function(value){
+			return ("+-*/^".indexOf(value) >= 0);
+		}
+
+		////////////////////////////////////////////////////////////////////////
+		// Public Interface IsOperand
+		// Tests whether an expression is a valid operand
+		////////////////////////////////////////////////////////////////////////
+		this.isOperand = function(value){
+			return !(isNaN(value));
+		}
+
+		////////////////////////////////////////////////////////////////////////
+		// Public Interface IsOperand
+		// Tests whether an expression is a valid operand
+		////////////////////////////////////////////////////////////////////////
+		this.explainer = function(){
+			return explainer;
+		}		
+
+
+		function logEval(expression, result){
+			var logItem = expression + "=" + result;
+			explainer.push(logItem);
+		}
+
+	}	
+
+	return lexar;
+});
+
---
+++ main.js
@@ -0,0 +1,45 @@
+/////////////////////////////////////////////////////////////////////////////////
+// Module 		: 		main.js
+// Description	: 		Main entry point for the progran. Loads Required modules
+//                      Handles page events via jquery and calls processing 
+//  					routines
+// Date			: 		Oct 2017
+// Author		:       Andrew Hall  
+/////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////////////////////
+// Require JS Module Configuration
+/////////////////////////////////////////////////////////////////////////////////
+require.config({
+ 	paths: {
+    	"jquery": "https://code.jquery.com/jquery-3.2.1.slim.min",
+    	"popper": "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min",
+    	"bootstrap": "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min",
+    	"lexar": "js/lexar",
+    	"stack": "js/stack"
+
+  	}
+});
+
+/////////////////////////////////////////////////////////////////////////////////
+// Event Handlers for the Page Using JQuery Dependency Injected via RequireJS
+/////////////////////////////////////////////////////////////////////////////////
+require(["jquery", "lexar"],function($, lexar){
+	
+	var exprLexar = new lexar();
+
+	$("#infixtopostfix").click(function(){
+    	$("#result").text("Infix Clicked");        	 
+	});
+
+	$("#postfixtoinfix").click(function(){
+		var postfix = $("#postfix").val();
+    	var result = exprLexar.evaluate(postfix);
+    	var explain = exprLexar.explainer();
+    	$("#result").text(result);  
+    	$("#explain").text(explain);      	 
+	});
+
+})
+
+
